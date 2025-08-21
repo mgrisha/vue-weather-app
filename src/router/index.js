542e83dd223b5918875ce82917f4a1b2ef78114c
grisha <grisha.endorphone@gmail.com>
2025-08-21 20:33:25 +0300

    add yml file

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '@/views/HomeView.vue'
 import CityView from '@/views/CityView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/vue-weather-app'),
   routes: [
     {
       path: '/',
